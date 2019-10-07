864b4cbe3a4d1db03a99911333c092d7bc168997
Kamuish <andremiguel952@gmail.com>
2019-10-07 19:26:18 +0100

    black fmt + TODOs

---
+++ archi/utils/data_export/export_photo_info.py
@@ -0,0 +1,33 @@
+import os
+
+
+def photo_SaveInfo(path, data_fits):
+    """
+    Stores information related to the photometric run.
+
+    In specific:
+        - which file was used
+        - mask type
+        - detect mode
+        - background grid
+        - all star related info
+    Parameters
+    ----------
+    path
+    data_fits
+
+    Returns
+    -------
+
+    """
+
+    with open(os.path.join(path, "photo_info.txt"), mode='w') as file:
+        file.write("Used file: {}".format(data_fits.used_file))
+        file.write("Mask type: {}\n".format(data_fits.mask_type))
+        file.write("Detect mode: {}\n".format(data_fits.detect_mode))
+        file.write("Background grid: {}\n".format(data_fits.bg_grid))
+
+        file.write("Star \t Cv \t Factors \n")
+
+        for j in data_fits.stars:
+            file.write("{} \t {} \t {}\n".format(j.number, j.calculate_cdpp(data_fits.mjd_time)[0], j.mask_factor))
