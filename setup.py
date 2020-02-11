c663959f0bfb54f20d94abd2e1b0a8720473e209
Kamuish <andremiguel952@gmail.com>
2020-02-11 21:24:53 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='pyarchi',
+      version='0.9',
+      description='archi',
+      url='http://github.com/Kamuish/archi',
+      author='Kamuish',
+      author_email='amiguel@astro.up.pt',
+      license='GNU General Public License v3.0',
+      packages=find_packages(), 
+      zip_safe=False)
