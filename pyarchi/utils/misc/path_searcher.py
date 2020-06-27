de457d506694bf25aace51936fdd93cdffa1d6e9
Kamuish <andremiguel952@gmail.com>
2020-06-27 17:06:13 +0100

    addind more info to the logs

--- pyarchi/utils/misc/path_searcher.py
+++ pyarchi/utils/misc/path_searcher.py
@@ -39,7 +39,6 @@ def path_finder(mode, off_curve=None, **kwargs):
 
         possible_paths = glob.glob(os.path.join(kwargs["base_folder"], extension_to_base))
         curve_2_search = kwargs["official_curve"] if not off_curve else off_curve
-
         regex_patern = re.compile(r"\S+{}\S+.fits".format(curve_2_search))
 
     elif mode == "stars":
@@ -52,7 +51,8 @@ def path_finder(mode, off_curve=None, **kwargs):
     try:
         return list(filter(regex_patern.findall, possible_paths))[0]
     except IndexError:
-        logger.critical("Failed to find DRP {} light curve".format(curve_2_search))
+        extra_info = '' if mode != 'default' else curve_2_search
+        logger.critical("Failed to find DRP {} output".format(mode +' - '+ extra_info))
         raise Exception("Missing the correct DRP output. Please verify the input folder") 
 
 
